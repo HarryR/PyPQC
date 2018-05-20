6f2f302f3723c1c38c23413fe0f14dcf7a3dcb0d
user <user@localhost.localdomain>
2018-05-20 06:59:21 +0100

    Cleanup pqc_cli, more modular, 'params' command

--- pqc_cli_api.c
+++ pqc_cli_api.c
@@ -42,6 +42,13 @@ struct hexarg_s {
 
 typedef struct hexarg_s hexarg_t;
 
+typedef int (*cmd_fn_t)( int argc, char **argv );
+
+typedef struct {
+    const char *name;
+    cmd_fn_t cmd;
+} cmd_t;
+
 
 static int hex2bin( const char *ptr, unsigned char *output_arg )
 {
@@ -154,152 +161,230 @@ void print_hex (unsigned char *data, size_t n)
 }
 
 
-int main( int argc, char **argv )
+int cmd_params( int argc, char **argv )
+{
+    printf("ALGNAME=%s\n", CRYPTO_ALGNAME);
+#ifdef CRYPTO_SECRETKEYBYTES
+    printf("CRYPTO_SECRETKEYBYTES=%d\n", CRYPTO_SECRETKEYBYTES);
+#endif
+#ifdef CRYPTO_PUBLICKEYBYTES
+    printf("CRYPTO_PUBLICKEYBYTES=%d\n", CRYPTO_PUBLICKEYBYTES);
+#endif
+#ifdef CRYPTO_BYTES
+    printf("CRYPTO_BYTES=%d\n", CRYPTO_BYTES);
+#endif
+#ifdef CRYPTO_CIPHERTEXTBYTES
+    printf("CRYPTO_CIPHERTEXTBYTES=%d\n", CRYPTO_CIPHERTEXTBYTES);
+#endif
+    return 0;
+}
+
+
+#ifdef BUILD_KEM
+
+int cmd_kem_gen( int argc, char **argv )
 {
     int ret_val;
-    unsigned char       seed[48];
+    unsigned char out_pk[CRYPTO_PUBLICKEYBYTES];
+    unsigned char out_sk[CRYPTO_SECRETKEYBYTES];
 
-    if( argc < 2 ) {
-        fprintf(stderr, "Usage: %s <operation> [arg [arg ...]]\n", argv[0]);
-        exit(1);
+    if ( (ret_val = crypto_kem_keypair(out_pk, out_sk)) != 0) {
+        fprintf(stderr, "crypto_kem_keypair returned <%d>\n", ret_val);
+        exit(6);
     }
+
+    printf("PK=");
+    print_hex(out_pk, sizeof(out_pk));
+    printf("\n");
+
+    printf("SK=");
+    print_hex(out_sk, sizeof(out_sk));
+    printf("\n");
+
+    return 0;
+}
+
+
+int cmd_kem_enc( int argc, char **argv )
+{
+    int ret_val;
+    unsigned char arg_pk[CRYPTO_PUBLICKEYBYTES];
+    unsigned char out_ct[CRYPTO_CIPHERTEXTBYTES];
+    unsigned char out_ss[CRYPTO_BYTES];
+
+    hexarg_t arg_spec[] = {
+        {"pk", sizeof(arg_pk), arg_pk}
+    };
+
+    if( ! parse_args(argc, argv, 1, arg_spec) )
+    {
+        exit(4);
+    }
+
+    if ( (ret_val = crypto_kem_enc(out_ct, out_ss, arg_pk)) != 0) {
+        fprintf(stderr, "crypto_kem_enc returned <%d>\n", ret_val);
+        exit(5);
+    }
+
+    printf("CT=");
+    print_hex(out_ct, sizeof(out_ct));
+    printf("\n");
+
+    printf("SS=");
+    print_hex(out_ss, sizeof(out_ss));
+    printf("\n");
+
+    return 0;
+}
+
+
+int cmd_kem_dec( int argc, char **argv )
+{
+    int ret_val;
+    unsigned char arg_ct[CRYPTO_CIPHERTEXTBYTES];
+    unsigned char arg_sk[CRYPTO_SECRETKEYBYTES];
+    unsigned char out_ss[CRYPTO_BYTES];
+
+    hexarg_t arg_spec[] = {
+        {"ct", sizeof(arg_ct), arg_ct},
+        {"sk", sizeof(arg_sk), arg_sk},
+    };
+
+    if( ! parse_args(argc, argv, 2, arg_spec) )
+    {
+        exit(3);
+    }
+
+    if ( (ret_val = crypto_kem_dec(out_ss, arg_ct, arg_sk)) != 0) {
+        fprintf(stderr, "crypto_kem_dec returned <%d>\n", ret_val);
+        exit(7);
+    }
+
+    printf("SS=");
+    print_hex(out_ss, sizeof(out_ss));
+    printf("\n");
+
+    return 0;
+}
+
+// BUILD_KEM
+#endif
+
+
+#ifdef BUILD_SIGN
+
+int cmd_sign_gen( int argc, char **argv )
+{
+    int ret_val;
+    unsigned char out_pk[CRYPTO_PUBLICKEYBYTES];
+    unsigned char out_sk[CRYPTO_SECRETKEYBYTES];
+
+    if ( (ret_val = crypto_sign_keypair(out_pk, out_sk)) != 0) {
+        fprintf(stderr, "crypto_sign_keypair returned <%d>\n", ret_val);
+        exit(7);
+    }
+
+    printf("PK=");
+    print_hex(out_pk, sizeof(out_pk));
+    printf("\n");
+
+    printf("SK=");
+    print_hex(out_sk, sizeof(out_sk));
+    printf("\n");
+
+    return 0;
+}
+
+
+int cmd_sign( int argc, char **argv )
+{
+    int ret_val;
+    unsigned char arg_sk[CRYPTO_SECRETKEYBYTES];
+    unsigned char *out_sm = 0;
+    unsigned long long out_smlen = 0;
+
+    hexarg_t arg_spec[] = {
+        {"sk", sizeof(arg_sk), arg_sk},
+        {"m", 0, 0},
+    };
+
+    if( ! parse_args(argc, argv, 2, arg_spec) )
+    {
+        exit(3);
+    }
+
+    out_sm = (unsigned char *)calloc(arg_spec[1].len + CRYPTO_BYTES, sizeof(unsigned char));
+
+    if ( (ret_val = crypto_sign(out_sm, &out_smlen, arg_spec[1].out, arg_spec[1].len, arg_sk)) != 0)
+    {
+        fprintf(stderr, "crypto_sign returned <%d>\n", ret_val);
+        exit(8);
+    }
+
+    printf("SM=");
+    print_hex(out_sm, sizeof(out_smlen));
+    printf("\n");
+
+    return 0;
+}
+
+
+int cmd_sign_open( int argc, char **argv )
+{
+    return 0;
+}
+
+// BUILD_SIGN
+#endif
+
+
+int main( int argc, char **argv )
+{
+    cmd_t cmds[] = {
+        {"params", cmd_params},
+
+#ifdef BUILD_KEM
+        {"kem-gen", cmd_kem_gen},
+        {"kem-enc", cmd_kem_enc},
+        {"kem-dec", cmd_kem_dec},
+#endif
+
+#ifdef BUILD_SIGN
+        {"sign-gen", cmd_sign_gen},
+        {"sign", cmd_sign},
+        {"sign-open", cmd_sign_open},
+#endif
+    };
+    int num_cmds = sizeof(cmds) / sizeof(cmd_t);
+
+    unsigned char seed[48];
 
     RAND_bytes(seed, sizeof(seed));
     randombytes_init(seed, NULL, 256);
 
-    if( 0 == strcmp(argv[1], "help") )
+    if( argc < 2 || 0 == strcmp(argv[1], "help") )
     {
+        fprintf(stderr, "Usage: %s <operation> [arg [arg ...]]\n", argv[0]);
 
-    }
-#ifdef BUILD_KEM
-    else if( 0 == strcmp(argv[1], "kem-gen") )
-    {
-        unsigned char out_pk[CRYPTO_PUBLICKEYBYTES];
-        unsigned char out_sk[CRYPTO_SECRETKEYBYTES];
+        fprintf(stderr, "\nOperations:\n");
 
-        if ( (ret_val = crypto_kem_keypair(out_pk, out_sk)) != 0) {
-            fprintf(stderr, "crypto_kem_keypair returned <%d>\n", ret_val);
-            exit(6);
-        }
-
-        printf("PK=");
-        print_hex(out_pk, sizeof(out_pk));
-        printf("\n");
-
-        printf("SK=");
-        print_hex(out_sk, sizeof(out_sk));
-        printf("\n");
-    }
-    else if( 0 == strcmp(argv[1], "kem-enc") )
-    {
-        unsigned char arg_pk[CRYPTO_PUBLICKEYBYTES];
-        unsigned char out_ct[CRYPTO_CIPHERTEXTBYTES];
-        unsigned char out_ss[CRYPTO_BYTES];
-
-        hexarg_t arg_spec[] = {
-            {"pk", sizeof(arg_pk), arg_pk}
-        };
-
-        if( ! parse_args(argc-2, &argv[2], 1, arg_spec) )
+        for( int i = 0; i < num_cmds; i++ )
         {
-            exit(4);
+            fprintf(stderr, "  %s\n", cmds[i].name);
         }
 
-        if ( (ret_val = crypto_kem_enc(out_ct, out_ss, arg_pk)) != 0) {
-            fprintf(stderr, "crypto_kem_enc returned <%d>\n", ret_val);
-            exit(5);
-        }
-
-        printf("CT=");
-        print_hex(out_ct, sizeof(out_ct));
-        printf("\n");
-
-        printf("SS=");
-        print_hex(out_ss, sizeof(out_ss));
-        printf("\n");
+        exit(1);
     }
-    else if( 0 == strcmp(argv[1], "kem-dec") )
+
+    for( int i = 0; i < num_cmds; i++ )
     {
-        unsigned char arg_ct[CRYPTO_CIPHERTEXTBYTES];
-        unsigned char arg_sk[CRYPTO_SECRETKEYBYTES];
-        unsigned char out_ss[CRYPTO_BYTES];
-
-        hexarg_t arg_spec[] = {
-            {"ct", sizeof(arg_ct), arg_ct},
-            {"sk", sizeof(arg_sk), arg_sk},
-        };
-
-        if( ! parse_args(argc-2, &argv[2], 2, arg_spec) )
+        if( 0 == strcmp(argv[1], cmds[i].name) )
         {
-            exit(3);
+            return cmds[i].cmd(argc-2, &argv[2]);
         }
-
-        if ( (ret_val = crypto_kem_dec(out_ss, arg_ct, arg_sk)) != 0) {
-            fprintf(stderr, "crypto_kem_dec returned <%d>\n", ret_val);
-            exit(7);
-        }
-
-        printf("SS=");
-        print_hex(out_ss, sizeof(out_ss));
-        printf("\n");
-    }
-// BUILD_KEM
-#endif
-#ifdef BUILD_SIGN
-    else if( 0 == strcmp(argv[1], "sign-gen") )
-    {
-        unsigned char out_pk[CRYPTO_PUBLICKEYBYTES];
-        unsigned char out_sk[CRYPTO_SECRETKEYBYTES];
-
-        if ( (ret_val = crypto_sign_keypair(out_pk, out_sk)) != 0) {
-            fprintf(stderr, "crypto_sign_keypair returned <%d>\n", ret_val);
-            exit(7);
-        }
-
-        printf("PK=");
-        print_hex(out_pk, sizeof(out_pk));
-        printf("\n");
-
-        printf("SK=");
-        print_hex(out_sk, sizeof(out_sk));
-        printf("\n");
-    }
-    else if( 0 == strcmp(argv[1], "sign") )
-    {
-        unsigned char arg_sk[CRYPTO_SECRETKEYBYTES];
-        unsigned char *out_sm = 0;
-        unsigned long long out_smlen = 0;
-
-
-        hexarg_t arg_spec[] = {
-            {"sk", sizeof(arg_sk), arg_sk},
-            {"m", 0, 0},
-        };
-
-        if( ! parse_args(argc-2, &argv[2], 2, arg_spec) )
-        {
-            exit(3);
-        }
-
-        out_sm = (unsigned char *)calloc(arg_spec[1].len + CRYPTO_BYTES, sizeof(unsigned char));
-
-        if ( (ret_val = crypto_sign(out_sm, &out_smlen, arg_spec[1].out, arg_spec[1].len, arg_sk)) != 0)
-        {
-            fprintf(stderr, "crypto_sign returned <%d>\n", ret_val);
-            exit(8);
-        }
-
-        printf("SM=");
-        print_hex(out_sm, sizeof(out_smlen));
-        printf("\n");
-    }
-// BUILD_SIGN
-#endif
-    else
-    {
-        fprintf(stderr, "Error: Unknown operation '%s'\n", argv[1]);
-        exit(2);
     }
 
-    return 0;
+    fprintf(stderr, "Error: unknown command '%s'\n", argv[1]);
+
+    return 1;
 }
